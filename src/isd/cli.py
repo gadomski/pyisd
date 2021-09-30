9a28c2fd86492544b3b49969b1be17c75d9e6e84
Pete Gadomski <pete.gadomski@gmail.com>
2021-09-30 09:04:40 -0600

    feat: Adds command line interface
    - Uses click
    - Right now just has one subcommand: `isd record`

---
+++ src/isd/cli.py
@@ -0,0 +1,28 @@
+# type: ignore
+
+import dataclasses
+import itertools
+import json
+
+import click
+from click import ClickException
+
+import isd
+
+
+@click.group()
+def main() -> None:
+    """Work with NOAA's Integrated Surface Database (ISD) from the command line."""
+
+
+@main.command()
+@click.argument("INFILE")
+@click.option("-i", "--index", default=0)
+def record(infile: str, index: int) -> None:
+    """Prints a single record to standard output in JSON format."""
+    with isd.open(infile) as records:
+        record = next(itertools.islice(records, index, None), None)
+        if record:
+            print(json.dumps(dataclasses.asdict(record), indent=4))
+        else:
+            raise ClickException(f"No record with index {index}")
