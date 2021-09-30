d27ed6590d8512ffb310a2d21a4f2e885ef0c73c
Pete Gadomski <pete.gadomski@gmail.com>
2021-09-30 09:26:17 -0600

    feat!: Adds optional checking
    Test coverage is incomplete, as not all positive/negative cases are
    covered by our one test file. Once we have serialization, it will be
    easier to set/check optionals.
    
    BREAKING CHANGE: Renamed `visibility_variation` to
    `visibility_variability` to better match documentation.

--- src/isd/record.py
+++ src/isd/record.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import Any, Callable, List, Optional, Tuple
 
 from isd.errors import IsdError
 
@@ -18,30 +18,30 @@ class Record:
     hour: int
     minute: int
     data_source: str
-    latitude: float
-    longitude: float
-    report_type: str
-    elevation: float
-    call_letters: str
+    latitude: Optional[float]
+    longitude: Optional[float]
+    report_type: Optional[str]
+    elevation: Optional[float]
+    call_letters: Optional[str]
     quality_control_process: str
-    wind_direction: int
+    wind_direction: Optional[int]
     wind_direction_quality_code: str
-    wind_observation_type: str
-    wind_speed: float
+    wind_observation_type: Optional[str]
+    wind_speed: Optional[float]
     wind_speed_quality_code: str
-    ceiling: int
+    ceiling: Optional[int]
     ceiling_quality_code: str
-    ceiling_determination_code: str
-    cavok_code: str
-    visibility: int
+    ceiling_determination_code: Optional[str]
+    cavok_code: Optional[str]
+    visibility: Optional[int]
     visibility_quality_code: str
-    visibility_variation_code: str
-    visibility_variation_quality_code: str
-    air_temperature: float
+    visibility_variability_code: Optional[str]
+    visibility_variability_quality_code: str
+    air_temperature: Optional[float]
     air_temperature_quality_code: str
-    dew_point_temperature: float
+    dew_point_temperature: Optional[float]
     dew_point_temperature_quality_code: str
-    sea_level_pressure: float
+    sea_level_pressure: Optional[float]
     sea_level_pressure_quality_code: str
     additional_data: str
     remarks: str
@@ -62,30 +62,31 @@ class Record:
         hour = int(line[23:25])
         minute = int(line[25:27])
         data_source = line[27]
-        latitude = float(line[28:34]) / 1000
-        longitude = float(line[34:41]) / 1000
-        report_type = line[41:46]
-        elevation = float(line[46:51])
-        call_letters = line[51:56]
+        # TODO test missing latitudes and longitudes
+        latitude = optional(line[28:34], "+99999", lambda s: float(s) / 1000)
+        longitude = optional(line[34:41], "+999999", lambda s: float(s) / 1000)
+        report_type = optional(line[41:46], "99999")
+        elevation = optional(line[46:51], "+9999", lambda s: float(s))
+        call_letters = optional(line[51:56], "99999")
         quality_control_process = line[56:60]
-        wind_direction = int(line[60:63])
+        wind_direction = optional(line[60:63], "999", lambda s: int(s))
         wind_direction_quality_code = line[63]
-        wind_observation_type = line[64]
-        wind_speed = float(line[65:69]) / 10
+        wind_observation_type = optional(line[64], "9")
+        wind_speed = optional(line[65:69], "9999", lambda s: float(s) / 10)
         wind_speed_quality_code = line[69]
-        ceiling = int(line[70:75])
+        ceiling = optional(line[70:75], "99999", lambda s: int(s))
         ceiling_quality_code = line[75]
-        ceiling_determination_code = line[76]
-        cavok_code = line[77]
-        visibility = int(line[78:84])
+        ceiling_determination_code = optional(line[76], "9")
+        cavok_code = optional(line[77], "9")
+        visibility = optional(line[78:84], "999999", lambda s: int(s))
         visibility_quality_code = line[84]
-        visibility_variation_code = line[85]
-        visibility_variation_quality_code = line[86]
-        air_temperature = float(line[87:92]) / 10
+        visibility_variability_code = optional(line[85], "9")
+        visibility_variability_quality_code = line[86]
+        air_temperature = optional(line[87:92], "+9999", lambda s: float(s) / 10)
         air_temperature_quality_code = line[92]
-        dew_point_temperature = float(line[93:98]) / 10
+        dew_point_temperature = optional(line[93:98], "+9999", lambda s: float(s) / 10)
         dew_point_temperature_quality_code = line[98]
-        sea_level_pressure = float(line[99:104]) / 10
+        sea_level_pressure = optional(line[99:104], "99999", lambda s: float(s) / 10)
         sea_level_pressure_quality_code = line[104]
         additional_data, remainder = extract_data(
             line[105:], "ADD", ["REM", "EQD", "QNN"]
@@ -121,8 +122,8 @@ class Record:
             cavok_code=cavok_code,
             visibility=visibility,
             visibility_quality_code=visibility_quality_code,
-            visibility_variation_code=visibility_variation_code,
-            visibility_variation_quality_code=visibility_variation_quality_code,
+            visibility_variability_code=visibility_variability_code,
+            visibility_variability_quality_code=visibility_variability_quality_code,
             air_temperature=air_temperature,
             air_temperature_quality_code=air_temperature_quality_code,
             dew_point_temperature=dew_point_temperature,
@@ -153,3 +154,14 @@ def extract_data(message: str, tag: str, later_tags: List[str]) -> Tuple[str, st
             return message[len(tag) :], ""
     else:
         return "", message
+
+
+def optional(
+    string: str, missing_value: str, transform: Optional[Callable[[str], Any]] = None
+) -> Any:
+    if string == missing_value:
+        return None
+    elif transform:
+        return transform(string)
+    else:
+        return string
