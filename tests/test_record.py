26ed278f7902f563118948a321061487bd3eb734
Pete Gadomski <pete.gadomski@gmail.com>
2021-09-30 09:26:17 -0600

    feat!: Adds optional checking
    Test coverage is incomplete, as not all positive/negative cases are
    covered by our one test file. Once we have serialization, it will be
    easier to set/check optionals.
    
    BREAKING CHANGE: Renamed `visibility_variation` to
    `visibility_variability` to better match documentation.

--- tests/test_record.py
+++ tests/test_record.py
@@ -18,26 +18,26 @@ def test_parse(record_line: str) -> None:
     assert record.longitude == -105.167
     assert record.report_type == "FM-15"
     assert record.elevation == 1541
-    assert record.call_letters == "99999"
+    assert record.call_letters is None
     assert record.quality_control_process == "V020"
-    assert record.wind_direction == 999
+    assert record.wind_direction is None
     assert record.wind_direction_quality_code == "9"
     assert record.wind_observation_type == "C"
     assert record.wind_speed == 0
     assert record.wind_speed_quality_code == "1"
     assert record.ceiling == 3353
     assert record.ceiling_quality_code == "1"
-    assert record.ceiling_determination_code == "9"
+    assert record.ceiling_determination_code is None
     assert record.cavok_code == "N"
     assert record.visibility == 16093
     assert record.visibility_quality_code == "1"
-    assert record.visibility_variation_code == "9"
-    assert record.visibility_variation_quality_code == "9"
+    assert record.visibility_variability_code is None
+    assert record.visibility_variability_quality_code == "9"
     assert record.air_temperature == 3.1
     assert record.air_temperature_quality_code == "1"
     assert record.dew_point_temperature == -5.8
     assert record.dew_point_temperature_quality_code == "1"
-    assert record.sea_level_pressure == 9999.9
+    assert record.sea_level_pressure is None
     assert record.sea_level_pressure_quality_code == "9"
     assert (
         record.additional_data == "GD14991+0335399GE19MSL   +99999+"
