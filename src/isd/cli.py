3af1fa5e762caebba11c129b87979bff22d5d619
Pete Gadomski <pete.gadomski@gmail.com>
2021-12-02 07:28:26 -0700

    refactor!: remove `open` from top-level module

--- src/isd/cli.py
+++ src/isd/cli.py
@@ -7,7 +7,7 @@ import json
 import click
 from click import ClickException
 
-import isd
+import isd.io
 
 
 @click.group()
@@ -20,7 +20,7 @@ def main() -> None:
 @click.option("-i", "--index", default=0)
 def record(infile: str, index: int) -> None:
     """Prints a single record to standard output in JSON format."""
-    with isd.open(infile) as records:
+    with isd.io.open(infile) as records:
         record = next(itertools.islice(records, index, None), None)
         if record:
             print(json.dumps(dataclasses.asdict(record), indent=4))
