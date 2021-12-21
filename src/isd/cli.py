eaab6b612e0e2e6c2df44de9bd3d9b59b79f9682
Pete Gadomski <pete.gadomski@gmail.com>
2021-12-21 08:53:15 -0700

    fix!: remove parquet support
    Closes #26

--- src/isd/cli.py
+++ src/isd/cli.py
@@ -3,15 +3,11 @@
 import dataclasses
 import itertools
 import json
-import os
-from typing import List, Optional
 
 import click
 from click import ClickException
-from dask.distributed import Client
 
 import isd.io
-import isd.parquet
 
 
 @click.group()
@@ -30,23 +26,3 @@ def record(infile: str, index: int) -> None:
             print(json.dumps(dataclasses.asdict(record), indent=4))
         else:
             raise ClickException(f"No record with index {index}")
-
-
-@main.command()
-@click.argument("INFILES", nargs=-1)
-@click.argument("DIRECTORY")
-@click.option("-c", "--client")
-def to_parquet(
-    infiles: List[str], directory: str, client: Optional[str] = None
-) -> None:
-    """Writes one or more ISD files to a parquet table."""
-    paths = []
-    for infile in infiles:
-        if os.path.isdir(infile):
-            for file_name in os.listdir(infile):
-                paths.append(os.path.abspath(os.path.join(infile, file_name)))
-        else:
-            paths.append(os.path.abspath(infile))
-    if client:
-        Client(client)
-    isd.parquet.write(paths, directory)
